75b48f0669f1382b25859e531a75d56f405e700b
Tong <tuytong6@gmail.com>
2021-01-26 00:34:50 +0700

    edit beatiful home

--- FoodManagement/src/app/menu/home/home.component.ts
+++ FoodManagement/src/app/menu/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public img : string = "https://cdn.discordapp.com/attachments/607168575509233704/803278491616542780/png-clipart-food-background-food-fruit.jpg" ;
   constructor() { }
 
   ngOnInit() {
